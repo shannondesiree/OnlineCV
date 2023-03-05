72956572234fd780c608e63451d1f90ab389ac89
Shannon McGurk <shannonmcgurk1@live.co.uk>
2023-03-05 21:00:31 +0000

    some progress in the interview section

---
+++ script.js
@@ -0,0 +1,10 @@
+const knowledgeQuestions = new Map([
+    ["What is meant by pseudocode?", 
+    "Pseudocode is a step-by-step way to represent an algorithm. It is a verbal outline that uses English and computer terminology and functions as an informal and transferrable guide to solving a problem."],
+    ["Please explain an algorithm. What are some of its important features?", 
+"An algorithm can be defined as a set of finite steps that when followed helps in accomplishing a particular task. Important features of an algorithm are clarity, efficiency, and finiteness."],
+    ["Please provide a brief explanation on variables.", 
+    "Variables are used for storing the input of a program as well as the computational results during program execution. These are actually named memory locations. The value stored in a variable can change during the program execution."]
+  ]);
+
+  console.log(knowledgeQuestions);
